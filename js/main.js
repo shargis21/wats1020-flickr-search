3aa35b16f2c603f82d1d19258bb7853b3c9bbafe
shargis21 <shargis21@gmail.com>
2017-02-13 12:47:41 -0500

    updated final js file

--- js/main.js
+++ js/main.js
@@ -19,18 +19,27 @@ $(document).on('ready', function(){
              $('#images').empty();
              $('h1.search-title').first()[0].innerHTML = "Search for: " + tags;
                  $.each(data.items, function( i, item ) {
-                 var newListItem = $("<li>")
-                 var newTitle = $('<p class="image-title">')
-                 var newDate = $('<p class="image-date">')
-                 var newDescription = $('<p class="image-description">')
-                 var newLink = $('<a>').attr('href', item.link)
-                 }).appendTo(newListItem);
-        newListItem.appendTo( "#images" );
-        if ( i === 15 ) {
-          return false;
-            }       
-       });
-   };
+                  var newListItem = $("<li>")
+                  var newTitle = $('<p class="image-title">').html(item.title).appendTo(newListItem);
+                  var newDate = $('<p class="image-date">').text(item.date_taken).appendTo(newListItem);
+                  var newDescription = $('<p class="image-description">').html(item.description).appendTo(newListItem);
+                  var newLink = $('<a>').attr('href', item.link).text('View on Flickr.').appendTo(newListItem);
+                   
+                 var newButton = $("<button class='btn btn-sm btn-primary'>enlarge</button>").attr({
+                    'data-title': item.title,
+                    'data-toggle': "modal",
+                    'data-target': "#infoModal",
+                    'data-imgsrc': item.media.m,
+                    'data-description': item.description,
+                    'type': "button"
+                  }).appendTo(newListItem);
+                  newListItem.appendTo( "#images" );
+                  if ( i === 15 ) {
+                    return false;
+        }
+      });
+    });
+  };
 
     // Attach an event to the search button (`button.search`) to execute the
     // search when clicked.
@@ -40,12 +49,19 @@ $(document).on('ready', function(){
     var searchTextInput = $(event.target.parentElement).find('input[name="searchText"]')[0];
     console.log(searchTextInput);
     searchImages(searchTextInput.value);
-  });           
+  });   
+  
+    $('#infoModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var title = button.data('title');
+    var imgSrc = button.data('imgsrc');
+    var imageDescription = button.data('description');
 
-
-    // STRETCH GOAL: Add a "more info" popup using the technique shown on the
-    // Bootstrap Modal documentation: http://getbootstrap.com/javascript/#modals-related-target
-
-
+    var modal = $(this);
+    modal.find('.modal-title').html(title);
+    var modalBody = modal.find('.modal-body');
+    modalBody.empty();
+    var modalDescription = $("<p class='image-description'>").html(imageDescription).appendTo(modalBody);
+  });
 
 });
